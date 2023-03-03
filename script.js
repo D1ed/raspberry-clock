f2c76d10ab584803c508db6bc21f6b053cb4fbb5
Lev Raspopov <lgraspopov@gmail.com>
2023-03-03 18:24:17 +0500

    сломаный

--- script.js
+++ script.js
@@ -30,8 +30,11 @@ body.onclick = function() {
       if (seconds < 10) seconds = '0' + seconds;
       clock.children[2].innerHTML = seconds;
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> parent of fe9021e (исправлена дата)
 =======
 >>>>>>> parent of fe9021e (исправлена дата)
     }
