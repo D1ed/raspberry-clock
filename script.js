54c867242d80ab701fb89e98b2575f08bb7af54f
Lev Raspopov <lgraspopov@gmail.com>
2024-10-29 23:53:54 +0500

    apiopenweather http -> https
    asda

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ setInterval(weatherBallon, 300000);
 
 
 function weatherBallon( cityID ) {
-  fetch('http://api.openweathermap.org/data/2.5/weather?id=' + '565778'+ '&appid=162204f36d92f27a55b386010cb2ccf1')  
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + '565778'+ '&appid=162204f36d92f27a55b386010cb2ccf1')  
   .then(function(resp) { return resp.json() }) // Convert data to json
   .then(function(data) {
     drawWeather(data);
