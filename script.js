90cc7152fe2e3000afd263462a621ca023ba68f1
Lev Raspopov <lgraspopov@gmail.com>
2024-10-29 23:04:11 +0500

    welcome back to 2y ago

--- script.js
+++ script.js
@@ -57,41 +57,40 @@ body.onclick = function() {
 
 
 
-
+weatherBallon();
 setInterval(weatherBallon, 300000);
-setInterval(drawWeather, 300000);
-  	const key = '';
+
+    const key = '';
 if(key=='') document.querySelector('.pogoda_container-1_temp').innerHTML = ('');
 
 function weatherBallon( cityID ) {
-	fetch('http://api.openweathermap.org/data/2.5/weather?id=' + '565778'+ '&appid=162204f36d92f27a55b386010cb2ccf1')  
-	.then(function(resp) { return resp.json() }) // Convert data to json
-	.then(function(data) {
-		drawWeather(data);
-	})
+  fetch('http://api.openweathermap.org/data/2.5/weather?id=' + '565778'+ '&appid=162204f36d92f27a55b386010cb2ccf1')  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    drawWeather(data);
+  })
 
-	.catch(function() {
-		// catch any errors
-	});
+  .catch(function() {
+    // catch any errors
+  });
 
 }
 function drawWeather( d ) {
   var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
   var description = d.weather[0].description; 
   var icon = d.weather[0].icon;
-	
-	document.querySelector('.pogoda_container_description').innerHTML = description;
-	document.querySelector('.pogoda_container-1_temp').innerHTML = celcius + '&deg;';
+  
+  document.querySelector('.pogoda_container_description').innerHTML = description;
+  document.querySelector('.pogoda_container-1_temp').innerHTML = celcius + '&deg;';
   document.querySelector('.now_temp').innerHTML = celcius + '&deg;';
-	document.querySelector('.pogoda_container-1_location').innerHTML = d.name;
+  document.querySelector('.pogoda_container-1_location').innerHTML = d.name;
   document.querySelector('.pogoda_container_icon').src = 'icons/' + icon + '.png';
   
 }
 
 
 setInterval(weatherBallonDaily, 3600000);
-setInterval(drawWeatherDaily, 3600000);
 
 
 function weatherBallonDaily() {
