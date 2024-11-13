77977d328e34486e6d75914a1cab56b530077fc9
Lev Raspopov <lgraspopov@gmail.com>
2024-11-13 13:40:12 +0500

    update script for free version

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function drawWeather( d ) {
 
 
 function weatherBallonDaily() {
-  fetch('http://api.weatherapi.com/v1/forecast.json?key=1f43449fc0804e9aa56182711242910&q=58.462971,56.399948&days=7&lang=ru')  
+  fetch('http://api.weatherapi.com/v1/forecast.json?key=1f43449fc0804e9aa56182711242910&q=58.462971,56.399948&days=3&lang=ru')  
   .then(function(resp) { return resp.json() }) // Convert data to json
   .then(function(data) {
     drawWeatherDaily(data);
@@ -147,13 +147,13 @@ function drawWeatherDaily( dnew ) {
   document.querySelector('.daily_after-tomorrow-two_where').innerHTML = convertTimestamp(uni_date_two);
   let uni_date_three = dnew.forecast.forecastday[2].date_epoch;
   document.querySelector('.daily_after-tomorrow-three_where').innerHTML = convertTimestamp(uni_date_three);
-  let uni_date_four = dnew.forecast.forecastday[3].date_epoch;
+  /*let uni_date_four = dnew.forecast.forecastday[3].date_epoch;
   document.querySelector('.daily_after-tomorrow-four_where').innerHTML = convertTimestamp(uni_date_four);
   let uni_date_five = dnew.forecast.forecastday[4].date_epoch;
   document.querySelector('.daily_after-tomorrow-five_where').innerHTML = convertTimestamp(uni_date_five);
   let uni_date_six = dnew.forecast.forecastday[5].date_epoch;
   document.querySelector('.daily_after-tomorrow-six_where').innerHTML = convertTimestamp(uni_date_six);
-
+  */
 
   var celciusnow_day_now = dnew.current.temp_c;
   var now_icon = dnew.current.condition.icon;
@@ -187,7 +187,7 @@ function drawWeatherDaily( dnew ) {
   document.querySelector('.daily_night-after-tomorrow-two').innerHTML = 'Ночью: ' + celciusnow_night_after_tomorrow_two + '&deg;';
   document.querySelector('.daily_icon-after-tomorrow-two').src = 'http:' + icon_after_tomorrow_two;
 
-  var celciusnow_day_after_tomorrow_three = dnew.forecast.forecastday[3].day.maxtemp_c;
+  /*var celciusnow_day_after_tomorrow_three = dnew.forecast.forecastday[3].day.maxtemp_c;
   var celciusnow_night_after_tomorrow_three = dnew.forecast.forecastday[3].day.mintemp_c;
   var description_after_tomorrow_three = dnew.forecast.forecastday[3].day.condition.text;
   var icon_after_tomorrow_three = dnew.forecast.forecastday[3].day.condition.icon;
@@ -222,7 +222,7 @@ function drawWeatherDaily( dnew ) {
   document.querySelector('.daily_day-after-tomorrow-six').innerHTML = 'Днём: ' + celciusnow_day_after_tomorrow_six + '&deg;';
   document.querySelector('.daily_night-after-tomorrow-six').innerHTML = 'Ночью: ' + celciusnow_night_after_tomorrow_six + '&deg;';
   document.querySelector('.daily_icon-after-tomorrow-six').src = 'http:' + icon_after_tomorrow_six;
-  
+  */
 }
 
 
